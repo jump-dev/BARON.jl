90ddf4343ff2351c8523959289db6a5e15d143f3
Oscar Dowson <odow@users.noreply.github.com>
2023-06-16 10:54:30 +1200

    Fix MOI. prefix in results.jl (#59)

--- test/moi_test.jl
+++ test/moi_test.jl
@@ -34,6 +34,7 @@ function test_runtests()
             "test_nonlinear_hs071_NLPBlockDual", # MathOptInterface.NLPBlockDual(1)
             "test_nonlinear_invalid",            # see below
             "test_linear_open_intervals",
+            "test_linear_variable_open_intervals",
             # returns NaN in expression and solver has to responde with:
             # MOI.get(model, MOI.TerminationStatus()) == MOI.INVALID_MODEL
             # this code will error when NaN is found (better than waiting to knoe about bad stuff)
